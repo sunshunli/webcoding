b7a2684cd2e0a02a35b25ef6ab4235a1d8f6e144
sunshunli <sunshunli@hotmail.com>
2017-02-01 20:48:19 +0800

    <!DOCTYPE html> <html lang="en"> <head>     <meta charset="UTF-8">     <title>Title</title> </head> <body>
    </body>

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>test</title>
+    <link rel="stylesheet" type="text/css" href="styles/style1/basic.css">
+    <link rel="stylesheet" type="text/css" href="styles/style1/index.css">
+</head>
+<body>
+
+    <div id="header">
+        <h1><a href="index.php">test</a></h1>
+        <ul>
+            <li>首页</li>
+            <li>注册</li>
+            <li>登录</li>
+            <li>个人中心</li>
+            <li>风格</li>
+            <li>管理</li>
+            <li>退出</li>
+        </ul>
+    </div>
+
+    <div id="list">
+        <h2>帖子列表</h2>
+    </div>
+
+    <div id="user">
+        <h2>新进会员</h2>
+    </div>
+
+    <div id="pics">
+        <h2>最新图片</h2>
+    </div>
+
+    <div id="footer">
+        <p>版权所有 翻版必究</p>
+        <p>本程序由<span>瓢城Web俱乐部</span>提供 源代码可以任意修改或发布 (c) yc60.com</p>
+    </div>
+
+</body>
+</html>
